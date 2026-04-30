96624ac271a07baa0770926f3e2943047d0e721c
Nilesh Shete <nileshshete2024@gmail.com>
2026-04-30 08:32:37 +0530

    minor name change

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div style={{ maxWidth: "800px", margin: "auto", padding: "20px" }}>
-      <h1>Mini Docs</h1>
+      <h1>Mini Docs in react18</h1>
 
       <input type="file" onChange={handleFileUpload} />
 
